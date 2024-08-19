03c3344d6f6d5297d7a9e450b894b66946264428
user <user@MacBook-Pro-de-user.local>
2024-08-18 22:57:56 -0400

    Jest fix

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js', 'html'],
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     transform: {
-      '^.+\\.(ts|js|html)$': 'ts-jest'
+      '^.+\\.(ts|js|html)$': 'ts-jest',
     },
+    moduleFileExtensions: ['ts', 'js', 'html'],
     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
     collectCoverage: true,
     coverageReporters: ['html', 'text-summary'],
